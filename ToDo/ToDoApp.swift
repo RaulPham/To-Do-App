6a745b4b229d164e989bdd01a23f32f591d1908a
Raul Pham <phatpham314@icloud.com>
2023-03-03 12:37:08 -0600

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Pham on 3/3/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
